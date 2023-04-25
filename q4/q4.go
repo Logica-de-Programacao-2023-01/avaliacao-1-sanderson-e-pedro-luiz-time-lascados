bef66c7759b6427cc523a309e36b4a641697548b
Sanderson Oliveira <84995084+Sanderson1406@users.noreply.github.com>
2023-04-25 10:27:15 -0300

    Update q4.go

--- q4/q4.go
+++ q4/q4.go
@@ -1,5 +1,7 @@
 package q4
 
+import "errors"
+
 //Uma loja virtual de roupas recebeu várias listas de produtos vendidos em diferentes dias da semana. O dono da loja
 //deseja analisar as listas para entender melhor o comportamento de suas vendas. Para isso, ele precisa classificar cada
 //lista como em ordem crescente, decrescente ou aleatória, de acordo com o preço dos produtos.
@@ -37,3 +39,5 @@ func ClassifyPrices(prices []int) (int, error) {
 
 	return 3, nil
 }
+
+
