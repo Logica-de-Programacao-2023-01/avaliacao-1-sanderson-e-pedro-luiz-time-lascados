f577ec9224796c24df5fcceebf3ec1f03e965c2c
Sanderson Oliveira <84995084+Sanderson1406@users.noreply.github.com>
2023-04-25 10:42:44 -0300

    Update bonus.go

--- bonus/bonus.go
+++ bonus/bonus.go
@@ -1,5 +1,10 @@
 package bonus
 
+import (
+	"fmt"
+	"sort"
+)
+
 //Joãozinho ganhou um kit de construção de torres. O kit é composto por várias barras de madeira, e seus comprimentos são
 //conhecidos. As barras podem ser empilhadas umas sobre as outras, desde que seus comprimentos sejam iguais.
 //
@@ -7,6 +12,32 @@ package bonus
 //barras da melhor maneira possível, determinando a altura da torre mais alta e quantas torres podem ser construídas.
 
 func CalculateTowers(barLengths []int) (int, int) {
-	// Seu código aqui
-	return 0, 0
+	sort.Ints(barLengths)
+	Max := 0
+	numTorres := 0
+
+	for i := 0; i < len(barLengths); {
+		h := 1
+		for j := i + 1; j < len(barLengths); j++ {
+			if barLengths[j] == barLengths[i] {
+				h++
+				i = j
+			}
+		}
+		Max = max(Max, h)
+		numTorres++
+		i++
+	}
+	return Max, numTorres
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func main() {
+	barras := []int{6, 5, 6, 7}
+	alturaMaxima, numTorres := CalculateTowers(barras)
+	fmt.Printf("%d altura máxima\n%d torres\n", alturaMaxima, numTorres)
 }
